0ab1914753bd2f0cd3e72c1d9334505b3f5206c3
Chris Youngs <v-chyou@microsoft.com>
2019-10-08 15:11:20 -0700

    Use new per-lesson functions, no enums

--- test.ts
+++ test.ts
@@ -1,3 +1,9 @@
 hourOfCode.analyze(SixDirection.Forward);
-hourOfCode.agentDetectFireHazard(FireHazards.DeadBush, SixDirection.Forward);
-hourOfCode.hazardsRemain();
+hourOfCode.agentDetectDeadBush(SixDirection.Forward);
+hourOfCode.agentDetectDryGrass(SixDirection.Forward);
+hourOfCode.hazardsRemainL4();
+hourOfCode.hazardsRemainL5();
+hourOfCode.hazardsRemainL6();
+hourOfCode.agentDestroyL4(SixDirection.Forward);
+hourOfCode.agentDestroyL5(SixDirection.Forward);
+hourOfCode.agentDestroyL6(SixDirection.Forward);
