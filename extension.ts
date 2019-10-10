6bb727feee03dcc80d8541826087634f9119b116
Chris Youngs <v-chyou@microsoft.com>
2019-10-10 11:42:21 -0700

    Block placed upon lesson completion
    A specific block is placed at a specific location in the world to let the world know that the player has completed the lesson.

--- extension.ts
+++ extension.ts
@@ -6,41 +6,50 @@ namespace hourOfCode {
     let targetsL4 = 5
     let targetsL5 = 8
     let targetsL6 = 12
+    let hazardA = Block.DeadBush
+    let hazardB = Block.Sunflower
+    let completionBlock = Block.DiamondBlock
+    let completionPosition = positions.createWorld(213, 99, 96)
 
     //% block="agent detect dead bush %dir"
     //% weight=80
     export function agentDetectDeadBush(dir: SixDirection) {
-        return agent.inspect(AgentInspection.Block, dir) == Block.DeadBush
+        return agent.inspect(AgentInspection.Block, dir) == hazardA
     }
 
     //% block="agent detect dry grass %dir"
     //% weight=80
     export function agentDetectDryGrass(dir: SixDirection) {
-        return agent.inspect(AgentInspection.Block, dir) == Block.Sunflower
+        return agent.inspect(AgentInspection.Block, dir) == hazardB
     }
 
     //% block="agent analyze %dir"
     //% weight=70
     export function agentAnalyze(dir: SixDirection) {
-        if (hourOfCode.agentDetectDeadBush(dir) || hourOfCode.agentDetectDryGrass(dir)) {
+        let targetBlock = agent.inspect(AgentInspection.Block, dir)
+        if (targetBlock == hazardA || targetBlock == hazardB) {
             mobs.execute(
                 mobs.target(TargetSelectorKind.NearestPlayer),
                 positions.create(0, 0, 0),
                 "playsound random.levelup @p"
             )
+            blocks.place(completionBlock, completionPosition)
         }
     }
 
     //% block="hazards remain"
     //% weight=45
     export function hazardsRemainL4() {
+        if (targetsL4 == 0) {
+            blocks.place(completionBlock, completionPosition)
+        }
         return targetsL4 > 0
     }
     //% block="agent destroy %dir"
     //% weight=40
     export function agentDestroyL4(dir: SixDirection) {
-        let targetBlock = agent.inspect(AgentInspection.Block, dir)
-        if (targetBlock == Block.DeadBush || targetBlock == Block.Sunflower) {
+        let targetBlock4 = agent.inspect(AgentInspection.Block, dir)
+        if (targetBlock4 == hazardA || targetBlock4 == hazardB) {
             targetsL4 -= 1
         }
         agent.destroy(dir)
@@ -49,13 +58,16 @@ namespace hourOfCode {
     //% block="hazards remain"
     //% weight=55
     export function hazardsRemainL5() {
+        if (targetsL5 == 0) {
+            blocks.place(completionBlock, completionPosition)
+        }
         return targetsL5 > 0
     }
     //% block="agent destroy %dir"
     //% weight=50
     export function agentDestroyL5(dir: SixDirection) {
-        let targetBlock = agent.inspect(AgentInspection.Block, dir)
-        if (targetBlock == Block.DeadBush || targetBlock == Block.Sunflower) {
+        let targetBlock5 = agent.inspect(AgentInspection.Block, dir)
+        if (targetBlock5 == hazardA || targetBlock5 == hazardB) {
             targetsL5 -= 1
         }
         agent.destroy(dir)
@@ -64,13 +76,16 @@ namespace hourOfCode {
     //% block="hazards remain"
     //% weight=65
     export function hazardsRemainL6() {
+        if (targetsL6 == 0) {
+            blocks.place(completionBlock, completionPosition)
+        }
         return targetsL6 > 0
     }
     //% block="agent destroy %dir"
     //% weight=60
     export function agentDestroyL6(dir: SixDirection) {
-        let targetBlock = agent.inspect(AgentInspection.Block, dir)
-        if (targetBlock == Block.DeadBush || targetBlock == Block.Sunflower) {
+        let targetBlock6 = agent.inspect(AgentInspection.Block, dir)
+        if (targetBlock6 == hazardA || targetBlock6 == hazardB) {
             targetsL6 -= 1
         }
         agent.destroy(dir)
