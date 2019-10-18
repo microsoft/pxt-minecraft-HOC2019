2e559a198e90af75e92e163a46f7d47ed7a10f28
Chris Youngs <v-chyou@microsoft.com>
2019-10-18 14:57:43 -0700

    Success = scoreboards, not blocks
    Code no longer places blocks to indicate success in the world; instead uses completeTask() to increment a value on the scoreboard to indicate success.
    Added openDoor() function to use in a new lesson that has been added to the world.
    Updated target values based on new world design.

--- extension.ts
+++ extension.ts
@@ -3,17 +3,41 @@
  */
 //% weight=94 color=#EC7505 icon="\uf1b3"
 namespace hourOfCode {
+    // the agentDestroy and hazardsRemain functions are supposed to
+    // look indistinguishable from lesson to lesson, since only one
+    // set will ever appear at a time. their display names should
+    // not specifically include which lesson they are a part of
     let targetsL4 = 5
-    let targetsL5 = 8
-    let targetsL6 = 12
-    let hazardA = 31    // fern for HoC (variants: tallgrass)
-    let hazardB = 175   // double tallgrass for HoC (variants: peony, rose bush, large fern, lilac, sunflower)
+    let targetsL5 = 10
+    let targetsL6 = 60
+    let shortHazard = 31   // fern and tallgrass
+    let tallHazard = 175   // double plant (variants: peony, rose bush, double tallgrass, large fern, lilac, sunflower)
     let airBlock = Block.Air
-    let completionBlockA = Block.DiamondBlock
-    let completionBlockB = Block.GoldBlock
-    let completionPosition = [positions.createWorld(-75, 65, -122), positions.createWorld(-57, 57, -63), positions.createWorld(-4, 32, 199)]
     let brokeNonHazard = false
+    let taskIsComplete = false
 
+    // hidden from user, used by other functions
+    //% block
+    function completeTask() {
+        if (!taskIsComplete) {
+            mobs.execute(
+                mobs.target(TargetSelectorKind.NearestPlayer),
+                positions.create(0, 0, 0),
+                "scoreboard players add @s task 1"
+            )
+            taskIsComplete = true
+        }
+    }
+    
+    /**
+     * Open a door
+     */
+    //% block="open door"
+    //% weight=90
+    export function openDoor() {
+        completeTask()
+    }
+    
     /**
      * Detect the hazard of a dry fern
      * @param dir the direction to detect the dry fern
@@ -21,7 +45,7 @@ namespace hourOfCode {
     //% block="agent detect dry fern %dir"
     //% weight=80
     export function agentDetectDryFern(dir: SixDirection) {
-        return agent.inspect(AgentInspection.Block, dir) == hazardA
+        return agent.inspect(AgentInspection.Block, dir) == shortHazard
     }
 
     /**
@@ -31,7 +55,7 @@ namespace hourOfCode {
     //% block="agent detect dry grass %dir"
     //% weight=80
     export function agentDetectDryGrass(dir: SixDirection) {
-        return agent.inspect(AgentInspection.Block, dir) == hazardB
+        return agent.inspect(AgentInspection.Block, dir) == tallHazard
     }
 
     /**
@@ -42,29 +66,24 @@ namespace hourOfCode {
     //% weight=70
     export function agentAnalyze(dir: SixDirection) {
         let targetBlock = agent.inspect(AgentInspection.Block, dir)
-        if (targetBlock == hazardA || targetBlock == hazardB) {
+        if (targetBlock == shortHazard || targetBlock == tallHazard) {
             mobs.execute(
                 mobs.target(TargetSelectorKind.NearestPlayer),
                 positions.create(0, 0, 0),
                 "playsound random.levelup @p"
             )
-            if (targetBlock == hazardA) {
-                blocks.place(completionBlockA, completionPosition[0])
-            } else {
-                blocks.place(completionBlockB, completionPosition[0])
-            }
-
+            completeTask()
         }
     }
 
     /**
      * Check for any remaining hazards on level 4
      */
-    //% block="hazards remain L4"
+    //% block="hazards remain"
     //% weight=45
     export function hazardsRemainL4() {
         if (targetsL4 == 0 && !brokeNonHazard) {
-            blocks.place(completionBlockA, completionPosition[1])
+            completeTask()
         }
         return targetsL4 > 0
     }
@@ -73,12 +92,11 @@ namespace hourOfCode {
      * Destroy block at level 4
      * @param dir the direction to destroy a block at
      */
-    //% block="agent destroy L4 %dir"
+    //% block="agent destroy %dir"
     //% weight=40
     export function agentDestroyL4(dir: SixDirection) {
-        loops.pause(500)
         let targetBlock4 = agent.inspect(AgentInspection.Block, dir)
-        if (targetBlock4 == hazardA || targetBlock4 == hazardB) {
+        if (targetBlock4 == shortHazard || targetBlock4 == tallHazard) {
             targetsL4 -= 1
         } else if (targetBlock4 != airBlock) {
             brokeNonHazard = true
@@ -89,11 +107,11 @@ namespace hourOfCode {
     /**
      * Check for any remaining hazards on level 5
      */
-    //% block="hazards remain L5"
+    //% block="hazards remain"
     //% weight=55
     export function hazardsRemainL5() {
         if (targetsL5 == 0 && !brokeNonHazard) {
-            blocks.place(completionBlockA, completionPosition[2])
+            completeTask()
         }
         return targetsL5 > 0
     }
@@ -102,11 +120,11 @@ namespace hourOfCode {
      * Destroy block at level 5
      * @param dir the direction to destroy a block at
      */
-    //% block="agent destroy L5 %dir"
+    //% block="agent destroy %dir"
     //% weight=50
     export function agentDestroyL5(dir: SixDirection) {
         let targetBlock5 = agent.inspect(AgentInspection.Block, dir)
-        if (targetBlock5 == hazardA || targetBlock5 == hazardB) {
+        if (targetBlock5 == shortHazard || targetBlock5 == tallHazard) {
             targetsL5 -= 1
         } else if (targetBlock5 != airBlock) {
             brokeNonHazard = true
@@ -117,11 +135,11 @@ namespace hourOfCode {
     /**
      * Check for any remaining hazards on level 6
      */
-    //% block="hazards remain L6"
+    //% block="hazards remain"
     //% weight=65
     export function hazardsRemainL6() {
         if (targetsL6 == 0 && !brokeNonHazard) {
-            blocks.place(completionBlockB, completionPosition[2])
+            completeTask()
         }
         return targetsL6 > 0
     }
@@ -130,11 +148,11 @@ namespace hourOfCode {
      * Destroy block at level 6
      * @param dir the direction to destroy a block at
      */
-    //% block="agent destroy L6 %dir"
+    //% block="agent destroy %dir"
     //% weight=60
     export function agentDestroyL6(dir: SixDirection) {
         let targetBlock6 = agent.inspect(AgentInspection.Block, dir)
-        if (targetBlock6 == hazardA || targetBlock6 == hazardB) {
+        if (targetBlock6 == shortHazard || targetBlock6 == tallHazard) {
             targetsL6 -= 1
         } else if (targetBlock6 != airBlock) {
             brokeNonHazard = true
